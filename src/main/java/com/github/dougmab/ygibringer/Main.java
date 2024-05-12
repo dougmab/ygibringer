320f10127bb1a5e41045dbff6c150101e31cb28b
Douglas Brum <douglas.mateus2005@gmail.com>
2024-05-11 23:30:35 -0300

    Remake views

--- src/main/java/com/github/dougmab/ygibringer/Main.java
+++ src/main/java/com/github/dougmab/ygibringer/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         ConfigurationService.load();
 
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/panel.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/app.fxml")));
         Scene scene = new Scene(root);
 
         stage.setResizable(false);
