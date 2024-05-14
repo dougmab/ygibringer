6ac55ed2a177e0f31f68de2532331575bc65a7a3
Douglas Brum <douglas.mateus2005@gmail.com>
2024-05-14 17:51:32 -0300

    Change status label

--- src/main/java/com/github/dougmab/ygibringer/app/controller/AccountController.java
+++ src/main/java/com/github/dougmab/ygibringer/app/controller/AccountController.java
@@ -2,9 +2,14 @@ package com.github.dougmab.ygibringer.app.controller;
 
 import com.github.dougmab.ygibringer.app.model.Account;
 import com.github.dougmab.ygibringer.app.model.Status;
+import com.github.dougmab.ygibringer.app.model.StatusType;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+
+import java.nio.file.Path;
 
 public class AccountController {
 
@@ -26,6 +31,15 @@ public class AccountController {
         loginLabel.setText(accountModel.getLogin());
         passwordLabel.setText(accountModel.getPassword());
         status = accountModel.getStatus();
+
+        if (!status.getType().equals(StatusType.PENDING)) {
+            changeStatusLabel(Status.pending(), status);
+        }
+
+
+        accountModel.statusProperty().addListener((obs, oldValue, newValue) -> {
+            changeStatusLabel(oldValue, newValue);
+        });
     }
 
     @FXML
@@ -35,6 +49,18 @@ public class AccountController {
 
     @FXML
     void editAccount(MouseEvent event) {
+
+    }
+
+    private void changeStatusLabel(Status oldStatus, Status newStatus) {
+        statusLabel.setText(newStatus.getTitle());
+
+        ImageView icon = (ImageView) statusLabel.getGraphic();
+        icon.setImage(new Image(Path.of("static", "img", newStatus.getType().getIconName()).toString()));
+
+        var classList = statusLabel.getStyleClass();
+        classList.remove(oldStatus.getType().getCssClass());
+        classList.add(newStatus.getType().getCssClass());
 
     }
 }
--- src/main/java/com/github/dougmab/ygibringer/app/model/StatusType.java
+++ src/main/java/com/github/dougmab/ygibringer/app/model/StatusType.java
@@ -1,5 +1,24 @@
 package com.github.dougmab.ygibringer.app.model;
 
 public enum StatusType {
-    PENDING, MANAGING, SUCCESS, ERROR
+    PENDING("pending-color", "pending.png"),
+    MANAGING("progress-color", "progress.png"),
+    SUCCESS("success-color", "success.png"),
+    ERROR("error-color", "error.png");
+
+    private final String cssClass;
+    private final String iconPath;
+
+    StatusType(String cssClass, String iconPath) {
+        this.cssClass = cssClass;
+        this.iconPath = iconPath;
+    }
+
+    public String getCssClass() {
+        return cssClass;
+    }
+
+    public String getIconName() {
+        return iconPath;
+    }
 }
